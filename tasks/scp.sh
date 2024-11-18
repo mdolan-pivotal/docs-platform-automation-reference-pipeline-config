ca02e28f1e6fa4669e618d2510e60ee05ef64d0c
Mike Dolan <michael.r.dolan@me.com>
2024-11-18 15:01:04 -0500

    WIP

--- tasks/scp.sh
+++ tasks/scp.sh
@@ -18,13 +18,13 @@ echo "${HOST_PRIVATE_KEY}" > /tmp/scp-key.pem
 chmod 600 /tmp/scp-key.pem
 
 # Set up SSH options
-SSH_OPTIONS="-i /tmp/scp-key.pem -o StrictHostKeyChecking=no"
+SSH_OPTIONS="-o StrictHostKeyChecking=no"
 
 # Determine direction of transfer based on PUSH_PULL
 if [ "$PUSH_PULL" == "push" ]; then
-    scp "${SSH_OPTIONS} ${LOCAL_PATH} ${HOST_USERNAME}@${HOST_FQDN}:${HOST_PATH}"
+    scp -i /tmp/scp-key.pem "${SSH_OPTIONS} ${LOCAL_PATH} ${HOST_USERNAME}@${HOST_FQDN}:${HOST_PATH}"
 elif [ "$PUSH_PULL" == "pull" ]; then
-    scp "${SSH_OPTIONS} ${HOST_USERNAME}@${HOST_FQDN}:${HOST_PATH} ${LOCAL_PATH}"
+    scp -i /tmp/scp-key.pem "${SSH_OPTIONS} ${HOST_USERNAME}@${HOST_FQDN}:${HOST_PATH} ${LOCAL_PATH}"
 else
     echo "Error: PUSH_PULL variable must be 'push' or 'pull'."
     exit 1
